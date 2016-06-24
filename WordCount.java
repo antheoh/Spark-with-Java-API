87b073a5b11ebaf701fb7891ee8381ead418bb81
Anastasis <anastasis.theoharis@gmail.com>
2016-06-24 16:49:00 +0300

    Fix #2

--- WordCount.java
+++ WordCount.java
@@ -86,7 +86,7 @@ public class WordCount {
     }
 
     //METHOD WORDS_MAPPER
-    //this method create key document-term and value 1 but we need to reduce,to mapping words
+    //this method create key document-term and value 1, but we need to reduce,to mapping words
     public static final FlatMapFunction<Tuple2<String, String>, Tuple2<Tuple2<String, String>, Integer>> WORDS_MAPPER = new FlatMapFunction<Tuple2<String, String>, Tuple2<Tuple2<String, String>, Integer>>() {
 
         public Iterable<Tuple2<Tuple2<String, String>, Integer>> call(Tuple2<String, String> stringIntegerTuple2) throws Exception {
